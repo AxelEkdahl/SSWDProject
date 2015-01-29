de77e737b18256c44004547fbcf8e0e97a3034b8
AxelEkdahl <axel_93_ekdahl@hotmail.com>
2015-01-29 15:59:24 +0100

    bla

--- SSWDProjStage/SSWDProjStage/App_Start/RouteConfig.cs
+++ SSWDProjStage/SSWDProjStage/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace SSWDProjStage
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Public", action = "IndexPublic", id = UrlParameter.Optional }
             );
         }
     }
